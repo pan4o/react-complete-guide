c05932854f5222f0fcb21feaef4252c2e717ff1b
pan4o <panchenko923@gmail.com>
2018-12-19 23:28:32 +0200

    add files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi I'm a React App!</h1>
+        <p>This is really working</p>
+        <Person name="Pancho" age="30">My Hobbie: Breaking</Person>
+        <Person name="Tom" age="33" />
+        <Person name="Bill" age="63" />
       </div>
     );
+    // return React.createElement(
+    //   'div',
+    //   null,
+    //   React.createElement(
+    //     'h1',
+    //     {
+    //       className: 'App'
+    //     },
+    //     'Hello man'
+    //   )
+    // );
   }
 }
 
